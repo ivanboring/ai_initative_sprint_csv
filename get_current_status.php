21b9659e4c53101919c817b9dba8d78001529550
Marcus Johansson <me@marcusmailbox.com>
2026-02-17 13:22:00 +0100

    Revert headless fetching

--- get_current_status.php
+++ get_current_status.php
@@ -292,15 +292,12 @@ function fetchAssignmentEvents($nid, $sprintStartDate, $sprintEndDate) {
         $browser = $browserFactory->createBrowser([
             'startupTimeout' => 60,
             'headless' => false,
-            'windowSize' => [1920, 1080],
-            'userAgent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
             'customFlags' => [
                 '--disable-blink-features=AutomationControlled',
                 '--disable-dev-shm-usage',
                 '--no-first-run',
                 '--no-default-browser-check',
                 '--disable-infobars',
-                '--headless=new', // Use new headless mode for better performance and compatibility
             ],
         ]);
 
